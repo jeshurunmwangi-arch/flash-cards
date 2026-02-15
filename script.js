1d848596e1ca813ca7ca0b04c0900b12de38edea
Jeshurun Mwangi <jeshurun.mwangi@student.moringaschool.com>
2026-02-15 19:43:14 +0300

    local storage

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+function saveToStorage(card) {
+    let cards = JSON.parse(localStorage.getItem("flashcards")) || [];
+
+    cards.push(card);
+
+    localStorage.setItem("flashcards",JSON.stringify(cards));
+}
+
+function loadFlashCards(container) {
+    let cards = JSON.parse(localStorage.getItem("flashcards")) || [];
+
+    cards.forEach(cardData => {
+        let card = new FlashCard(
+            cardData.subject,
+            cardData.question,
+            cardData.answer
+        );
+    })
+}
+
+
+
+
 function flashCard (subject,question,answer) {
     this.subject = subject;
     this.question = question;
+    this.answer = answer;
 }
+
